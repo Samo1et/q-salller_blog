88b4d26fa2897105b317b68dde3ecdcd63d5ea60
d.samoylis <d.samoylis@cardsmobile.ru>
2023-06-07 12:05:32 +0700

    add react frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import App from "./containers/App";
+
+import client from "./utils/apolloClient";
+import "./style.css"
+
+ReactDOM.render(
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById("root")
+);
